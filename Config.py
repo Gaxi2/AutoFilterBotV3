b3a517b5ed9e9e18c52093e5af9bf49039d81ef1
Gaxi2 <103119685+Gaxi2@users.noreply.github.com>
2022-04-16 04:44:26 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -35,9 +35,9 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Messages
 default_start_msg = """
-**Hi, I'm Auto Filter V3**
+**Hi, I'm Finder V3**
 
-Here you can search files in Inline mode as well as PM, Use the below buttons to search files or send me the name of file to search.
+Here You Can Get Files Requested In FindHub, Join Our Updates Channel To Know More.
 """
 START_MSG = environ.get('START_MSG', default_start_msg)
 
